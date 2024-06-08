89601a060446e5b430377b43c61e11a89abb0c8a
Fr. Josh McCarty <fr.mccarty@gmail.com>
2024-06-08 14:23:15 -0500

    hmmm

--- routes/web.php
+++ routes/web.php
@@ -18,15 +18,10 @@ use Illuminate\Support\Facades\Schema;
 
 Route::get('/', Landing::class);
 
-//if (Schema::hasTable('apps')) {
-//    $apps = App::get();
-//
-//    if($apps) {
-//        foreach($apps as $app) {
-//            Route::get('/' . $app->url, \App\Livewire\Detail::class);
-//        }
-//    }
-//}
+$apps = App::get();
+foreach($apps as $app) {
+    Route::get('/' . $app->url, \App\Livewire\Detail::class);
+}
 
 Route::middleware([
     'auth:sanctum',
