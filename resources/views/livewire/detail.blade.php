888624830c3b1c4643708f8dd03446ae9f000a6e
Fr. Josh McCarty <fr.mccarty@gmail.com>
2024-06-10 08:17:50 -0500

    fixed header size problem on large format.

--- resources/views/livewire/detail.blade.php
+++ resources/views/livewire/detail.blade.php
@@ -1,38 +1,45 @@
 <div>
-    <div class="md:px-[10em] px-6 w-full bg-gray-800">
-        <div class="pt-8">
-            <a href="/" class="flex items-center mb-3 text-white">
-                <x-icon.left-arrow/>
-                <span>Home</span>
-            </a>
-        </div>
-
-        <div class="pb-[4em] xl:flex items-center md:gap-[10em]">
-            @if($app->custom_logo)
-                <div class="bg-white p-3 rounded-xl w-48 h-48 flex items-center justify-center">
-                    <img alt="Logo" class="w-[7em]" src="{{$app->custom_logo}}" />
+    <div class="w-full bg-gray-800">
+        <x-page>
+            <div class="md:px-[4em] px-6">
+                <div class="pt-8">
+                    <a href="/" class="flex items-center mb-3 text-white">
+                        <x-icon.left-arrow/>
+                        <span>Home</span>
+                    </a>
                 </div>
-            @else
-                    <i class="fas fa-{{$app->font_awesome}} text-white text-[6em] py-12"></i>
-            @endif
 
-            <div class="text-white ">
-                <div class="text-[5em] font-bold">
-                    {{$app->name}}
-                </div>
-                <div class="text-[2em]">
-                    {{$app->description}}
+                <div class="pb-[4em] xl:flex items-center md:gap-[10em]">
+                    @if($app->custom_logo)
+                        <div class="bg-white p-3 rounded-xl w-48 h-48 flex items-center justify-center">
+                            <img alt="Logo" class="w-[7em]" src="{{$app->custom_logo}}" />
+                        </div>
+                    @else
+                            <i class="fas fa-{{$app->font_awesome}} text-white text-[6em] py-12"></i>
+                    @endif
+
+                    <div class="text-white ">
+                        <div class="text-[5em] font-bold">
+                            {{$app->name}}
+                        </div>
+                        <div class="text-[2em]">
+                            {{$app->description}}
+                        </div>
+
+                        <x-link.white-button class="mt-8" target="_blank" href="{{$app->public_url}}">
+                            Go To app
+                        </x-link.white-button>
+                    </div>
                 </div>
             </div>
-        </div>
 
+
+
+        </x-page>
     </div>
 
     <x-page>
         <div class="gap-[5em] px-[4em] py-10 ">
-            <x-link.black-button class="" target="_blank" href="{{$app->public_url}}">
-                Go To app
-            </x-link.black-button>
             <div class="bg-white py-6 px-6 mt-8">
                 <div class="markdown">
                     {!! Str::markdown($app->content)  !!}
--- resources/views/livewire/landing.blade.php
+++ resources/views/livewire/landing.blade.php
@@ -1,25 +1,29 @@
 <div>
-    <div class="md:px-[10em] px-6 py-[4em] md:gap-[10em] bg-gray-800 w-full md:flex items-center">
-        <i class="fas fa-book-bible text-white text-[10em]"></i>
+    <div class="py-[5em] bg-gray-800 w-full">
+        <x-page>
+            <div class="md:gap-[10em] px-[4em] md:flex items-center">
+                <i class="fas fa-book-bible text-white text-[10em]"></i>
 
-        <div class="text-white ">
-            <div class="text-[5em] font-bold">
-                Catholic Resource
+                <div class="text-white ">
+                    <div class="text-[5em] font-bold">
+                        Catholic Resource
+                    </div>
+                    <div class="text-[2em]">
+                        Useful Tech for Church people and other Entrepreneurs
+                    </div>
+                    <div class="mt-10">
+                        <x-link.white-button target="_blank" href="https://lolekproductions.org">
+                            Lolek Productions
+                        </x-link.white-button>
+                    </div>
+                </div>
             </div>
-            <div class="text-[2em]">
-                Useful Tech for Church people and other Entrepreneurs
-            </div>
-            <div class="mt-10">
-                <x-link.white-button target="_blank" href="https://lolekproductions.org">
-                    Lolek Productions
-                </x-link.white-button>
-            </div>
-        </div>
+        </x-page>
     </div>
 
     <x-page>
         @foreach($apps as $index => $app)
-            <div class="{{ $index % 2 != 0 ? 'flex-row-reverse' : '' }} gap-[5em] md:flex justify-between px-[4em] py-[4em]  items-center">
+            <div class="{{ $index % 2 != 0 ? 'flex-row-reverse' : '' }} gap-[5em] md:flex justify-between px-[4em] py-[4em] items-center">
                 <div>
                     <div class="text-[3em] font-semibold">
                         {{$app->name}}
