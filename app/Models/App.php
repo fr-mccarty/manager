a7dafbe5dd0abc23f521f6f7cc9fa6c15bbe7dbf
Fr. Josh McCarty <fr.mccarty@gmail.com>
2024-06-11 13:28:27 -0500

    added search on apps

--- app/Models/App.php
+++ app/Models/App.php
@@ -10,6 +10,15 @@ class App extends Model
     public string $moduleNamePlural = 'Apps';
     public string $moduleUrl = 'apps';
 
+    public function scopeSearch($query, string $terms = null)
+    {
+        collect(explode(' ', $terms))->filter()->each(function ($term) use ($query) {
+            $term = '%' . $term . '%';
+            $query->where('name', 'like', $term)
+                ->orWhere('description', 'like', $term);
+        });
+    }
+
     // Boolean
     public function setIsActiveAttribute($value) { $this->attributes["is_active"] = ($value ? 1 : 0); }
 
