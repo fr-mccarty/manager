c8937e6b8c990294d3450d6d1031754da0b921fd
Fr. Josh McCarty <fr.mccarty@gmail.com>
2024-06-11 15:02:12 -0500

    try favicon again

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -12,7 +12,7 @@
         <link rel="icon" type="image/png" sizes="16x16" href="/public/favicon/favicon-16x16.png">
 
         <!-- Apple Touch Icon for iOS devices -->
-        <link rel="apple-touch-icon" sizes="180x180" href="/public/favicon/apple-touch-icon-180x180.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="/public/favicon/apple-touch-icon.png">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
